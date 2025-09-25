6f7db85b3471f77be584c4a6df80435f88fd1ac3
Vipul-Kataria <vipulkataria8337@outlook.com>
2025-09-25 21:08:00 +0530

    created segmented buttons

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,4 +1,7 @@
+import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
+
+enum Auth { signin, signup }
 
 class AuthScreen extends StatefulWidget {
   static const String routeName = "/auth-screen";
@@ -9,8 +12,45 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.signup;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Hello i am new page ")));
+    return Scaffold(
+      backgroundColor: GlobalVariables.greyBackgroundCOlor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text(
+                "Welcome",
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: SegmentedButton<Auth>(
+                  segments: [
+                    const ButtonSegment(
+                      value: Auth.signup,
+                      label: Text("Create Account"),
+                    ),
+                    const ButtonSegment(
+                      value: Auth.signin,
+                      label: Text("Sign-In"),
+                    ),
+                  ],
+                  selected:{_auth},
+                  onSelectionChanged: (Set newSelection) {
+                    setState(() {
+                      _auth = newSelection.first;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
